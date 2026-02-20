d80796310f27985c22290c9881bc55f4af38f495
Artheek18 <artheek2321.Shan@gmail.com>
2026-02-20 15:35:33 -0500

    adding new site

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,7 +17,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",  # Vite dev server
         "http://127.0.0.1:5173",
-        "https://pdf-template-black.vercel.app", # Vercel deployment
+        "https://pdf-template-woad.vercel.app", # Vercel deployment
     ],
     allow_credentials=True,
     allow_methods=["*"],
